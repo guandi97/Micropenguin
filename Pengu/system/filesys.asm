28bf29c39262bdb964ceafbd99b02ed644d26a15
Justin <jyang772@gmail.com>
2014-03-05 23:56:27 -0800

    Added sexy title. Re-write filesystem

--- Pengu/system/filesys.asm
+++ Pengu/system/filesys.asm
@@ -5,108 +5,169 @@
 ;Print each file name
 
 
+list_directory:
+	mov cx,	0			; Counter
 
-ls: 
+	mov ax, dirlist			; Get list of files on disk
+	call os_get_file_list
 
-pusha				;Save registers
-call reset_floppy_   ;Make sure floppy controller starts reading from first sector
+	mov si, dirlist
+	mov ah, 0Eh			; BIOS teletype function
 
+.repeat:
+	lodsb				; Start printing filenames
+	cmp al, 0			; Quit if end of string
+	je .done
 
-;call ReadSectors           ;EX:BX points to buffer
+	cmp al, ','			; If comma in list string, don't print it
+	jne .nonewline
+	pusha
+	call os_print_newline		; But print a newline instead
+	popa
+	jmp .repeat
 
-;mov ah, 2                  ;For INT 13h
-;mov al, [datasectors]      ;Read from beginning of datasectors
+.nonewline:
+	int 10h
+	jmp .repeat
 
-						   ;Location of memory to read root from
-;int 13h
+.done:
+	call os_print_newline
+	jmp get_cmd
+	
+;;=====================================================
+_os_get_file_list:
+	pusha
 
+	;mov word [.file_list_tmp], ax
 
-;Just like parsing the root directory for our kernel
-;We do the same for all files
-mov cx, WORD [bpbRootEntries_]  
-;mov di, 0x200                  ;This is where root directory starts in PenguOS
-;push si                        ;Save SI, this register will be used for LODSB
+	;mov eax, 0			; Needed for some older BIOSes
 
-mov si, lol
-call f_Print
+	call disk_reset_floppy		; Just in case disk was changed
 
+	;mov ax, 19			; Root dir starts at logical sector 19  (Reserved Sectors(1) + FATs( 2 * 9))
+	;call disk_convert_l2hts
 
-mov di, 0x7c00
-mov si, 0x200
-push ax
+	;mov si, disk_buffer		; ES:BX should point to our buffer
+	;mov bx, si
 
+	mov si, 0x200
+	mov bx, 0x7c00
+	
+	
+	mov ah, 2			; Params for int 13h: read floppy sectors
+	mov al, 18			; And read 14 of them
 
-
-mov es, di
-
-mov     cx, WORD [bpbRootEntries_]             ; load loop counter
-          mov     di, 0x0200                            ; locate first root entry
-     .LOOP:
-          push    cx
-          mov     cx, 0x000B                            ; eleven character name
-          mov     si, ImageName_                         ; image name to find
-          push    di
-     rep  cmpsb                                         ; test for entry match
-          pop     di                                    ;Restore value of DI
-          je      _Printaws
-          pop     cx
-          add     di, 0x0020                            ; queue next directory entry
-          loop    .LOOP
-
-
-
-
-
-
-
-
-retf
-
-;===============================================================
-;Needs to be put in some separate include file
-
-        reset_floppy_:
-		mov ah, 0
-		mov dl, 0
-		int 0x13
-		jc reset_floppy_
-		ret
-		  
-		  
-		f_PrintLine:
-		mov ah, 0x0E
-		int 0x10      ; otherwise, print out the character!
-		ret
-		
-		_Printaws:
-		mov si, lol
-		call f_Print
-		ret
-;FAIL:
-     
-          ;mov     si, msgFailure_
-          ;call    _Print
-          ;mov     ah, 0x00
-          ;int     0x16                                ; await keypress
-          ;int     0x19                                ; warm boot computer
+	pusha				; Prepare to enter loop
 
 
+.read_root_dir:
+	popa
+	pusha
 
-		  f_Print:
-	lodsb					; load next byte from string from SI to AL
-	or			al, al		; Does AL=0?
-	jz			f_PrintDone	; Yep, null terminator found-bail out
-	mov			ah,	0eh		; Nope-Print the character
-	int			10h
-	jmp			f_Print		; Repeat until null terminator found
-f_PrintDone:
-	ret				; we are done, so return
+	stc
+	int 13h				; Read sectors
+	;call disk_reset_floppy		; Check we've read them OK
+	;jnc .show_dir_init		; No errors, continue
+	
+	call .show_dir_init
+	
+	;call disk_reset_floppy		; Error = reset controller and try again
+	;jnc .read_root_dir
+	;jmp .done			; Double error, exit 'dir' routine
 
-;================================
-file_buffer times 64 db 0
-msgFailure_	db "System failed to load directory...Rebooting",0
-lol			db "You are awesome, you found me.",0
-ImageName_ db "KERNEL  BIN"
-tmp_list		dw 0
+.show_dir_init:
+	popa
 
-bpbRootEntries_: 	DW 224
+	mov ax, 0
+	;mov si, disk_buffer		; Data reader from start of filenames
+
+	mov word di, 0 ;[.file_list_tmp]	; Name destination buffer
+
+
+.start_entry:
+	mov al, [si+11]			; File attributes for entry
+	cmp al, 0Fh			; Windows marker, skip it
+	je .skip
+
+	test al, 18h			; Is this a directory entry or volume label?
+	jnz .skip			; Yes, ignore it
+
+	mov al, [si]
+	cmp al, 229			; If we read 229 = deleted filename
+	je .skip
+
+	cmp al, 0			; 1st byte = entry never used
+	je .done
+
+
+	;mov cx, 1			; Set char counter
+	mov dx, si			; Beginning of possible entry
+	mov cx, 11
+	
+.testdirentry:
+	inc si
+	mov al, [si]			; Test for most unusable characters
+	cmp al, ' '			; Windows sometimes puts 0 (UTF-8) or 0FFh
+	jl .nxtdirentry
+	cmp al, '~'
+	ja .nxtdirentry
+
+	;inc cx
+	;cmp cx, 11			; Done 11 char filename?
+	;je .gotfilename
+	;jmp .testdirentry
+	loop .testdirentry
+	
+	
+
+.gotfilename:				; Got a filename that passes testing
+	mov si, dx			; DX = where getting string
+
+	mov cx, 0
+.loopy:
+	mov byte al, [si]
+	cmp al, ' '
+	je .ignore_space
+	mov byte [di], al
+	inc si
+	inc di
+	inc cx
+	cmp cx, 8
+	je .add_dot
+	cmp cx, 11
+	je .done_copy
+	jmp .loopy
+
+.ignore_space:
+	inc si
+	inc cx
+	cmp cx, 8
+	je .add_dot
+	jmp .loopy
+
+.add_dot:
+	mov byte [di], '.'
+	inc di
+	jmp .loopy
+
+.done_copy:
+	mov byte [di], ','		; Use comma to separate filenames
+	inc di
+
+.nxtdirentry:
+	mov si, dx			; Start of entry, pretend to skip to next
+
+.skip:
+	add si, 32			; Shift to next 32 bytes (next filename)
+	jmp .start_entry
+
+
+.done:
+	dec di
+	mov byte [di], 0		; Zero-terminate string (gets rid of final comma)
+
+	popa
+	ret
+
+
+	.file_list_tmp		dw 0
