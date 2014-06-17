4cc1471cd2431ad4948343538fc6e0b81525d865
Guandi97 <alex@micropenguin.net>
2014-06-17 13:26:33 -0400

    finalized, not fixing bugs as this is only an experiment

--- Experiments/xJoodle/threadrrr/numid/daBoss.java
+++ Experiments/xJoodle/threadrrr/numid/daBoss.java
@@ -2,37 +2,95 @@ import java.util.*;
 import java.lang.*;
 
 class daBoss {
+	
 	public static void main(String args[]) {
 		Stack<nomnom> thrdz=new Stack<nomnom>();
-		int sp=0;
+		int si=0,di=0;
+		Scanner sc=new Scanner(System.in);
+		String uin;
+		String ain[];
+		neo nemo=new neo();
+		nemo.dalock=new Object();
+
+		System.out.println("^c quit");
+		while(true) {
+			uin=sc.nextLine();
+			ain=uin.split(" ");
+			if(uin.matches("^spawn\\s\\d+$")) {
+				di+=spawndem(nemo,Integer.parseInt(ain[1]),thrdz,di);
+			}
+			else if(uin.matches("^kill\\s\\d+$")) {
+				si+=Integer.parseInt(ain[1]);
+				killdem(Integer.parseInt(ain[1]),thrdz);
+			}
+			else if(uin.matches("^add(\\s\\d+){3}$")) {
+				addem(Integer.parseInt(ain[1]),Integer.parseInt(ain[2]),Integer.parseInt(ain[3]),thrdz,nemo.dalock);
+			}
+			else if(uin.matches("^assign(\\s\\d+){2}$")) {
+				assdem(Integer.parseInt(ain[1]),Integer.parseInt(ain[2]),si,thrdz,nemo);
+			}
+			else if(uin.matches("^view\\s\\d+$")) {
+				System.out.println(givme(thrdz,Integer.parseInt(ain[1]),nemo));
+			}
+			else if(uin.matches("^list$")) {
+				listem(thrdz,si,di);
+			}
+			else if(uin.matches("help")) {
+				System.out.println("spawn # #\nkill #\nadd # # #\nassign # #\nview #\nlist");
+			}
+			else { System.out.println("command not found"); }
+		}
+
 	}
-	public static int spawndem(int quan,Stack<nomnom> thrdz) {
+	public static int spawndem(neo nemo,int quan,Stack<nomnom> thrdz,int si) {
 		int i;
 		for(i=0;i<quan;i++) {
-			thrdz.push(new nomnom());
+			thrdz.push(new nomnom(nemo));
 			thrdz.get(thrdz.size()-1).start();
+			System.out.format("Thread %d\n",si+i);
 		}
 		return i;
 	}
-	public static void killdem(int quan,Stack<nomnom> thrdz) {
-		for(quan;quan!=0;quan--) {
+	public static int killdem(int quan,Stack<nomnom> thrdz) {
+		for(quan=quan;quan!=0;quan--) {
 			thrdz.get(quan).stop();
-			thrdz.get(quan).join();
+			try {
+				thrdz.get(quan).join();
+			} catch(InterruptedException e) {}
 			thrdz.remove(quan);
 		}
+		return quan;
 	}
-	public static void addem(int a,int b,int c,Stack<nomnom>thrdz) {
-		thrdz.get(a).note=2;
-		thrdz.get(b).note=2;
-		thrdz.get(c).note=3;
-		notifyAll();
+	public static void addem(int a,int b,int c,Stack<nomnom>thrdz,Object dalock) {
+		neo nemo=new neo();
+		nemo.datar=0;
+		thrdz.get(a).note=3;
+		thrdz.get(b).note=3;
+		thrdz.get(c).note=4;
+		synchronized(dalock) {
+			dalock.notifyAll();
+		}
 	}
-	public static void assdem(int thrd,int num,Stack<nomnom> thrdz) {
-		thrdz.get(thrd).note=1;
-		notifyAll();
+	public static void assdem(int thrd,int num,int si,Stack<nomnom> thrdz,neo nemo) {
+		nemo.datar=num;
+		thrdz.get(thrd-si).note=2;
+		synchronized(nemo.dalock) {
+			nemo.dalock.notifyAll();
+		}
 	}
-	public static int givme(int id) {
-		thrdz.get(id).note=0;
-		notifyAll();
+	public static int givme(Stack<nomnom> thrdz,int id,neo nemo) {
+		thrdz.get(id).note=1;
+		synchronized(nemo.dalock) {
+			nemo.dalock.notifyAll();
+			try {
+				nemo.dalock.wait();
+			} catch(InterruptedException e) {}
+		}
+		return nemo.datar;
+	}
+	public static void listem(Stack<nomnom> thrdz,int si,int di) {
+		for(int i=si;i<di;i++) {
+			System.out.format("Thread %d: %d\n",i,thrdz.get(i-si).ratad);
+		}
 	}
 }
--- Experiments/xJoodle/threadrrr/numid/minionz.java
+++ Experiments/xJoodle/threadrrr/numid/minionz.java
@@ -1,20 +1,61 @@
+import java.lang.*;
+
 class nomnom extends Thread {
-	int i;
-	public static int note=0;
-	static Object dalock;
-	public static void nomnom(Object lelock) {
-		dalock=lelock;
+	int ratad=0;
+	volatile boolean terminateflg=false;
+	public volatile int note=0;
+	neo nemo;
+
+	public nomnom(neo nao) {
+		nemo=nao;
 	}
 	public void run() {	
+		while(terminateflg==false) {
+			synchronized(nemo.dalock) {
+				while(note==0 && terminateflg==false) {
+					try {
+						nemo.dalock.wait();
+					} catch(InterruptedException e) {}
+				}
+			}
+			switch(note) {
+				case 1: giver(); break;
+				case 2: storr(); break;
+				case 3:	addr(); break;
+				case 4: getr(); break;
+			}
+			note=0;
+		}
+		return;
 	}
-	public void addr() {
+	public void giver() {
+		neo.datar=ratad;
+		synchronized(nemo.dalock) {
+			nemo.dalock.notifyAll();
+		}
+	}
+	public synchronized void addr() {
+		neo.datar+=ratad;
+		neo.i++;
 	}
 	public void storr() {
+		ratad=neo.datar;
 	}
-	public void printr() {
+	public void getr() {
+		synchronized(nemo.dalock) {
+			while(neo.i>2) {
+				try {
+					nemo.dalock.wait();
+				} catch(InterruptedException e) {}
+			}
+		}
+		ratad=neo.datar;
+		neo.i=0;
 	}
 }
 class neo {
-	volatile static int d=0;
+	Object dalock;
+	public volatile static int datar=0;
+	public volatile static int i=0;
 }
 
