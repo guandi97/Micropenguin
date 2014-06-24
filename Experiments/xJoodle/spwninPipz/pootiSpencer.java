b6062fee9df417e6928d2413b8da3f772d5fef7b
guandi97 <alex@micropenguin.net>
2014-06-24 13:59:10 -0400

    sync

--- Experiments/xJoodle/spwninPipz/pootiSpencer.java
+++ Experiments/xJoodle/spwninPipz/pootiSpencer.java
@@ -1,15 +1,23 @@
 import java.util.*;
 import java.lang.*;
-import java.ni.channels.*;
+import java.io.*;
 
-class pootiSpencer extends Thread {
-	Pipe sandvitch=new Pipe();
+class pootiSpencer {
 
 	public static void main(String args[]) {
+		PipedInputStream heavy=new PipedInputStream();		//read
+		try {
+			PipedOutputStream rain=new PipedOutputStream(heavy);
+		} catch(IOException e) {}
+
+		dispencer naow=new dispencer(heavy);
+		dispencer sandvitch=newDispencer(rain);
+		naow.start();
+		sandvitch.start();
 	}
 
-	public static void pstore {
+	public static void pstore() {
 	}
-	public static void psend {
+	public static void psend() {
 	}
 }
