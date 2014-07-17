cc28542ab5bc921e57e8b51d906d93d3fdfeb10b
Guandi97 <alex@micropenguin.net>
2014-07-17 09:06:55 -0700

    l

--- Experiments/xJoodle/prototype02/ioNdFile.java
+++ Experiments/xJoodle/prototype02/ioNdFile.java
@@ -4,15 +4,20 @@ class ioNdFile {
 	public static RandomAccessFile cmdreadrr;
 
 	public static String readScptLn() {
-		String ln;
+		String ln=null;
 		do {
 			try {
 				ln=cmdreadrr.readLine().trim();
 			} catch(IOException e) {
 				System.err.println(e);
+			} catch(NullPointerException e) {	//trim() will trigger this
+				System.err.println("EOF");
+				ln=null;
+				return ln;
 			}
 		} while(ln.matches("(^$|^\\/\\/.*$)"));
-		ln=ln.replace(""\\/\\/.*$","");
+		ln=ln.replace("\\/\\/.*$","");
+		System.err.println(ln);
 		return ln;
 	}
 }
--- Experiments/xJoodle/prototype02/x02.java
+++ Experiments/xJoodle/prototype02/x02.java
@@ -1,8 +1,9 @@
 //handles initial parsing
+import java.io.*;
 
 class nooope {
 
-	private String ln;
+	private static String ln=null;
 
 	public static void main(String[] args) {
 		//method objects
@@ -12,7 +13,11 @@ class nooope {
 		String[] arrbuff=null;
 
 		//public objects
-		cmdreadrr=new RandomAccessFile(args[0],"r");
+		try {
+			ioNdFile.cmdreadrr=new RandomAccessFile(args[0],"r");
+		} catch(IOException e) {
+			System.err.println(e);
+		}
 
 		//initialization of the "static" members
 		variFun variFun=new variFun();
@@ -21,7 +26,28 @@ class nooope {
 
 		//initial readln
 		ln=ioNdFile.readScptLn();
-		while(ln!=EOF) {
+		while(ln!=null) {
+			//function
+			if(ln.matches("^\\s*\\b(\\w|\\d)+\\{((\\$(\\w|\\d)+\\[\\d+]|(\\w|\\d)+)*\\s*,\\s*)*((\\$(\\w|\\d)+\\[\\d+]|(\\w|\\d)+)\\s*)\\)$")) /*}*/ {
+			}
+			//variable
+			else if(ln.matches("\\b(\\w|\\d)+=.*")) {
+			}
+			//cmdsequence
+			else if(ln.matches("\\$\\(.+\\)\\s*$")) {
+			}
+			//brackets
+			else if(ln.matches("\\{\\s*$")) /*}*/ {
+			}
+		/*{*/	else if(ln.matches("}\\s*$")) {
+			}
+			else {
+				System.out.println("you fail");
+				System.exit(0);
+			}
+
+			//subsequent reads
 			ln=ioNdFile.readScptLn();
 		}
+	}
 }
